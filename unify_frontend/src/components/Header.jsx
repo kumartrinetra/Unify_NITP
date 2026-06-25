d0ea48b071f11d9cf2d9a9626e1ed3b82a66b99b
Trinetra Kumar <trinetrakumar7250@gmail.com>
2026-06-25 10:54:07 +0530

    Changes Using LLM

--- unify_frontend/src/components/Header.jsx
+++ unify_frontend/src/components/Header.jsx
@@ -117,20 +117,22 @@ const Header = () => {
               </div>
             </li>
             <li>
-              <button
+              <a
+                href="javascript:void(0)"
                 onClick={() => {}}
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Explore
-              </button>
+              </a>
             </li>
             <li>
-              <button
+              <a
+                href="javascript:void(0)"
                 onClick={() => {}}
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Upcoming
-              </button>
+              </a>
             </li>
             <li>
               <a
